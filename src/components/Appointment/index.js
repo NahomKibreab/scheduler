1e8cbcd03d85372ded8e1c17537bd38cfa2da4da
NahomKibreab <nknahom@gmail.com>
2021-09-01 22:50:06 -0600

    Displaying Appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,14 @@
 import React from "react";
+import Empty from "./Empty";
+import Header from "./Header";
+import Show from "./Show";
 import "./styles.scss";
 
 export default function Appointment(props) {
-  return <article className="appointment"></article>;
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? <Show {...props.interview} /> : <Empty />}
+    </article>
+  );
 }
