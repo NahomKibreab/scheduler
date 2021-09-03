6ef35ed9e0ceb9be19bba08330a9a0756c2e822f
Nahom Mehanzel <nknahom@gmail.com>
2021-09-03 06:00:59 +0000

    Combined all state to manage all

--- src/components/Application.js
+++ src/components/Application.js
@@ -58,8 +58,14 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+  });
+
+  const setDay = (day) => setState((prev) => ({ ...prev, day }));
+  const setDays = (days) => setState((prev) => ({ ...prev, days }));
 
   useEffect(() => {
     axios.get("/api/days").then((res) => {
@@ -86,7 +92,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} day={day} setDay={setDay} />
+          <DayList days={state.days} day={state.day} setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
---
+++ src/helpers/selectors.js
@@ -0,0 +1,16 @@
+export function getAppointmentsForDay(state, day) {
+  let apptForDay;
+  if (state.days.length === 0) {
+    return [];
+  }
+  state.days.forEach((item) => {
+    if (item.name === day) {
+      apptForDay = item.appointments;
+    }
+  });
+  if (!apptForDay) {
+    return [];
+  }
+  const appointment = apptForDay.map((appt) => state.appointments[appt]);
+  return appointment;
+}
